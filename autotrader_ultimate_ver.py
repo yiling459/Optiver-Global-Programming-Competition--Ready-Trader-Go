e41fc4f5b69c3148c0e297c53c1ccf40f91b7d24
Yiling Cao <161507617+yiling459@users.noreply.github.com>
2024-03-05 01:31:04 +0800

    Add files via upload

--- autotrader_ultimate_ver.py
+++ autotrader_ultimate_ver.py
@@ -17,7 +17,8 @@
 #     <https://www.gnu.org/licenses/>.
 import asyncio
 import itertools
-
+import pickle
+import numpy as np
 from typing import List
 
 from ready_trader_go import BaseAutoTrader, Instrument, Lifespan, MAXIMUM_ASK, MINIMUM_BID, Side
@@ -59,6 +60,17 @@ class AutoTrader(BaseAutoTrader):
             #writer=csv.writer(file)
             #writer.writerow(['ask_prices','ask_volumes','bid_prices','bid_volumes'])
 
+        self.para = np.array([4.99325349e-01,4.99325349e-01,-1.63716662e-01,1.63717294e-01,
+        7.11188053e-04,-3.24373155e-04,-3.56691402e-02, -3.34521759e-02,
+         -2.47186476e-02, -2.70819467e-02, -2.21935806e-02, 1.71802162e-02])
+        self.para2=np.array([4.98896958e-01, 4.98896958e-01, -3.28393549e-01, 3.27647082e-01,
+        1.67055391e-03,-1.94629469e-04, -3.05817559e-02, -3.46504459e-02,
+        -2.19845297e-02, -2.52628973e-02, -2.01023961e-02,1.58686716e-02])
+        self.para3=np.array([
+            4.98612058e-01,  4.98612058e-01, -4.93371940e-01,  4.90853370e-01,
+  3.64612492e-03, -3.14051903e-04, -2.51046112e-02, -3.57968672e-02,
+ -1.92988411e-02, -2.29557092e-02, -1.80078616e-02,  1.44601284e-02,
+        ])
     def on_error_message(self, client_order_id: int, error_message: bytes) -> None:
         """Called when the exchange detects an error.
 
@@ -104,8 +116,76 @@ class AutoTrader(BaseAutoTrader):
         
         row_total=[instrument,sequence_number,ask_prices,ask_volumes,bid_prices,bid_volumes,self.big_bill_bid_FUTURE,self.big_bill_ask_FUTURE,self.big_bill_bid_ETF,self.big_bill_ask_ETF,difference]
 
+
+        #make prediction to the next market price change, next next market price change and next nexst next market price change using the trained model
+        if instrument==Instrument.ETF:
+            print('ETF')
+            '''input_lst=[ask_prices[0],ask_prices[1],ask_prices[2],
+                       ask_prices[3],ask_prices[4],ask_volumes[1],
+                       ask_volumes[2],ask_volumes[3],ask_volumes[4],ask_volumes[5],difference]
+            array_input=np.array(input_lst)'''
+            current_market_value=(ask_prices[0]+bid_prices[0])/2
+            difference=sum(ask_volumes)-sum(bid_volumes)
+        # print(ask_prices[0],ask_prices[1],ask_prices[2],
+        #                ask_prices[3],ask_prices[4],ask_volumes[1],
+        #                ask_volumes[2],ask_volumes[3],ask_volumes[4],ask_volumes[5],difference)
+            input_lst=[ask_prices[0],ask_prices[0],ask_prices[1],ask_prices[2],
+                       ask_prices[3],ask_prices[4],ask_volumes[0],
+                       ask_volumes[1],ask_volumes[2],ask_volumes[3],ask_volumes[4],difference]
+            print(current_market_value)
+            print(input_lst)
+            print(self.para)
+            array_input=np.array(input_lst)
+            pred = np.dot(array_input,self.para) + 149.21758120827144
+            print("333")
+            self.logger.info("prediction:" + str(pred))
+            print(pred)
+            pred2=np.dot(array_input,self.para2)+171.80880311250803
+            self.logger.info('prediction 2:'+str(pred2))
+            print(pred2)
+            pred3=np.dot(array_input,self.para3)+187.99534604750806
+            #self.logger.info('prediction 3:'+str(pred3))
+            print(pred3)
+            if pred>current_market_value and (pred2<pred or pred3<pred) and self.position>-POSITION_LIMIT:
+                self.ask_id=next(self.order_ids)
+                print('1')
+                price_adjustment = 2
+                print('2')
+                new_ask_price = ask_prices[0] + price_adjustment if ask_prices[0] != 0 else 0
+                print('3')
+                self.send_insert_order(self.ask_id,Side.SELL,new_ask_price,LOT_SIZE,Lifespan.FILL_AND_KILL)
+                print('4')
+                self.asks.add(self.ask_id)
+                print('5')
+            if pred3>pred2 and pred2>pred and pred>current_market_value and self.position>-POSITION_LIMIT:
+                self.ask_id=next(self.order_ids)
+                print('1')
+                price_adjustment=5
+                print('2')
+                new_ask_price=ask_prices[0]+price_adjustment
+                print('3')
+                self.send_insert_order(self.ask_id,Side.SELL,new_ask_price,LOT_SIZE,Lifespan.GOOD_FOR_DAY)
+                print('4')
+                self.asks.add(self.ask_id)
+                print('5')
+            if pred<current_market_value and (pred2>pred or pred3>pred) and self.position<POSITION_LIMIT:
+                self.bid_id = next(self.order_ids)
+                price_adjustment=2
+                new_bid_price=bid_prices[0]-price_adjustment
+                self.send_insert_order(self.bid_id,Side.BUY,new_bid_price,LOT_SIZE,Lifespan.FILL_AND_KILL)
+                self.bids.add(self.bid_id)
+                print('done')
+            if pred3<pred2 and pred2<pred and pred<current_market_value and self.position<POSITION_LIMIT:
+                self.bid_id = next(self.order_ids)
+                price_adjustment=5
+                new_bid_price=bid_prices[0]-price_adjustment
+                self.send_insert_order(self.bid_id,Side.BUY,new_bid_price,LOT_SIZE,Lifespan.GOOD_FOR_DAY)
+                self.bids.add(self.bid_id)
+                print('done')
+            print('done')
         #write the data into the csv file
-        with open('output4.csv',mode='a',newline='') as file:
+        
+        '''with open('output4.csv',mode='a',newline='') as file:
             writer=csv.writer(file)
             writer.writerow(row_total)
             
@@ -118,9 +198,11 @@ class AutoTrader(BaseAutoTrader):
             row_FUTURE=[instrument,sequence_number,ask_prices,ask_volumes,bid_prices,bid_volumes,self.big_bill_ask_FUTURE,self.big_bill_bid_FUTURE,difference]
             with open('outputFuture4.csv',mode='a',newline='') as file:
                 writer=csv.writer(file)
-                writer.writerow(row_FUTURE)
+                writer.writerow(row_FUTURE)'''
         
         if instrument == Instrument.FUTURE:
+            
+            print('future')
             price_adjustment = (self.position // LOT_SIZE) * TICK_SIZE_IN_CENTS
             #here we change + to - in order to buy at a lower price
             new_bid_price = bid_prices[0] - price_adjustment if bid_prices[0] != 0 else 0
